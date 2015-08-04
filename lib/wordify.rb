c2a84d585b0414ba881e52c451435a592ead53c2
Austin Kincaid & Terry Marr <student@epicodus.com>
2015-08-04 16:44:18 -0700

    Refactor code to seperate controller from inner look_up loop

--- lib/wordify.rb
+++ lib/wordify.rb
@@ -1,65 +1,27 @@
 class Fixnum
   define_method(:wordify) do
-    ones = Hash.new()
-    ones.store(0,"")
-    ones.store(1,"one")
-    ones.store(2,"two")
-    ones.store(3,"three")
-    ones.store(4,"four")
-    ones.store(5,"five")
-    ones.store(6,"six")
-    ones.store(7,"seven")
-    ones.store(8,"eight")
-    ones.store(9,"nine")
-    ones.store(10,"ten")
-    ones.store(11,"eleven")
-    ones.store(12,"twelve")
-    ones.store(13,"thirteen")
-    ones.store(14,"fourteen")
-    ones.store(15,"fifteen")
-    ones.store(16,"sixteen")
-    ones.store(17,"seventeen")
-    ones.store(18,"eighteen")
-    ones.store(19,"nineteen")
-    ones.store(20,"twenty ")
-    ones.store(30,"thirty ")
-    ones.store(40, "forty ")
-    ones.store(50,"fifty ")
-    ones.store(60, "sixty ")
-    ones.store(70, "seventy ")
-    ones.store(80, "eighty ")
-    ones.store(90, "ninety ")
-
+    require('wordify_lookup')
     words = ""
     newnum = self
+    numstr = self.to_s
+    sections = numstr.length()
 
-    if newnum.>(20)
+    case sections
+    when  1..3
+        words = newnum.wordify_lookup()
+      when 4..6
+        newnum = numstr[3,5].to_i()
+        words = newnum.wordify_lookup().concat(" thousand ")
+        newnum = numstr[0,2].to_i()
+        words.concat(newnum.wordify_lookup())
 
-      input_one = newnum % 10
-      input_thousand = newnum.div(1000)
-      newnum = newnum - input_thousand.*(1000)
-      input_hundred = newnum.div(100)
-      newnum = newnum - input_hundred.*(100)
-      input_ten = newnum.div(10).*(10)
+      when 7..9
 
-      if input_thousand.>(0)
-        words.concat(ones.fetch(input_thousand))
-        words.concat(" thousand ")
-      end
+      when 10..12
 
-      if input_hundred.>(0)
-        words.concat(ones.fetch(input_hundred))
-        words.concat(" hundred ")
-      end
-
-      words.concat(ones.fetch(input_ten))
-      if input_one.>(0)
-        words.concat(ones.fetch(input_one))
-      end
-    else
-      words.concat(ones.fetch(newnum))
+      when 13
+        
     end
-
-    return words.strip()
+    words
   end
 end
