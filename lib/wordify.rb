cc3482af06ad26f897041c665d12bdc0d34233c0
Austin Kincaid & Terry Marr <student@epicodus.com>
2015-08-04 12:15:10 -0700

    test run to 11

--- lib/wordify.rb
+++ lib/wordify.rb
@@ -11,7 +11,16 @@ class Fixnum
     ones.store(8,"eight")
     ones.store(9,"nine")
     ones.store(10,"ten")
-
+    ones.store(11,"eleven")
+    ones.store(12,"twelve")
+    ones.store(13,"thirteen")
+    ones.store(14,"fourteen")
+    ones.store(15,"fifteen")
+    ones.store(16,"sixteen")
+    ones.store(17,"seventeen")
+    ones.store(18,"eighteen")
+    ones.store(19,"nineteen")
+    ones.store(20,"twenty")
     ones.fetch(self)
 
   end
--- spec/wordify_spec.rb
+++ spec/wordify_spec.rb
@@ -7,4 +7,8 @@ describe('Fixnum#wordify') do
     expect(1.wordify()).to(eq("one"))
   end
 
+  it('takes a number to the tens place and returns word') do
+    expect(11.wordify()).to(eq("eleven"))
+  end
+
 end
