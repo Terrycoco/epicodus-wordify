a86517c4e9a3a4f2d4a127ad6e34e89329b32d66
Austin Kincaid & Terry Marr <student@epicodus.com>
2015-08-04 16:56:00 -0700

    make code work with leading zeroes

--- lib/wordify.rb
+++ lib/wordify.rb
@@ -3,16 +3,16 @@ class Fixnum
     require('wordify_lookup')
     words = ""
     newnum = self
-    numstr = self.to_s
+    numstr = self.to_s().reverse()
     sections = numstr.length()
 
     case sections
     when  1..3
         words = newnum.wordify_lookup()
       when 4..6
-        newnum = numstr[3,5].to_i()
+        newnum = numstr[3..5].reverse().to_i()
         words = newnum.wordify_lookup().concat(" thousand ")
-        newnum = numstr[0,2].to_i()
+        newnum = numstr[0..2].reverse().to_i()
         words.concat(newnum.wordify_lookup())
 
       when 7..9
@@ -20,7 +20,7 @@ class Fixnum
       when 10..12
 
       when 13
-        
+
     end
     words
   end
--- spec/wordify_spec.rb
+++ spec/wordify_spec.rb
@@ -1,40 +1,39 @@
 require('rspec')
 require('wordify')
-require('wordify_lookup')
 require('pry')
 
 describe('Fixnum#wordify') do
 
-  # it('takes number to ones place and returns word') do
-  #   expect(1.wordify()).to(eq("one"))
-  # end
-  #
-  # it('takes a number to the tens place and returns word') do
-  #   expect(11.wordify()).to(eq("eleven"))
-  # end
+  it('takes number to ones place and returns word') do
+    expect(1.wordify()).to(eq("one"))
+  end
+
+  it('takes a number to the tens place and returns word') do
+    expect(11.wordify()).to(eq("eleven"))
+  end
 
   it('takes a number above 20 (below 30) and returns the word twenty and the ones digit') do
-    expect(021.wordify_lookup()).to(eq("twenty one"))
+    expect(21.wordify()).to(eq("twenty one"))
   end
- #
- #  it('takes a number above 30 (below 100) and returns an appropriate string') do
- #    expect(32.wordify()).to(eq("thirty two"))
- #  end
- #
- #  it("takes a number over 99 and returns the number followed by 'hundred'") do
- #    expect(100.wordify()).to(eq("one hundred"))
- #  end
- #
- #  it("takes a complex number over 100 (and less than 1000) and returns an appropriate string") do
- #   expect(101.wordify()).to(eq("one hundred one"))
- #  end
- #
- #  it("takes a number over 1000 and returns the words") do
- #    expect(1021.wordify()).to(eq("one thousand twenty one"))
- #  end
- #
- #  it("takes a complex number over 1000 and returns the number") do
- #   expect(123456.wordify()).to(eq("one hundred twenty three thousand four hundred fifty six"))
- # end
+
+  it('takes a number above 30 (below 100) and returns an appropriate string') do
+    expect(32.wordify()).to(eq("thirty two"))
+  end
+
+  it("takes a number over 99 and returns the number followed by 'hundred'") do
+    expect(100.wordify()).to(eq("one hundred"))
+  end
+
+  it("takes a complex number over 100 (and less than 1000) and returns an appropriate string") do
+   expect(101.wordify()).to(eq("one hundred one"))
+  end
+
+  it("takes a number over 1000 and returns the words") do
+    expect(1021.wordify()).to(eq("one thousand twenty one"))
+  end
+
+  it("takes a complex number over 1000 and returns the number") do
+   expect(123456.wordify()).to(eq("one hundred twenty three thousand four hundred fifty six"))
+ end
 
 end
